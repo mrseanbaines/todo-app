4ae2a0e893c2c61adb664b494cb71fa307b2dfac
Sean Baines <mrseanbaines@gmail.com>
2017-10-31 23:35:46 +0000

    WIP base component and start of styles

---
+++ src/components/base.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="pad-2">
+    <div class="pad">
+      <input
+        class="full-width"
+        type="text"
+        placeholder="Try typing something..."
+        v-model="text"
+        v-on:blur="isNull"
+        v-on:focus="isNull"
+        v-bind:class="{ hide: isEmpty }"
+      >
+    </div>
+    <div class="pad text-center">
+      <span v-on:click="addTodo" class="fa fa-plus"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      text: '',
+      isEmpty: false
+    }
+  },
+  methods: {
+    addTodo (ev) {
+      
+    },
+    isNull () {
+      if (this.text) {
+        this.isEmpty = !this.isEmpty
+      }
+    }
+  }
+}
+</script>
